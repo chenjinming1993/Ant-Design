92c25d3ed987c6eed0248ca7b2aff02d2e5b1240
cjm <cjm@wooduan2019>
2020-10-30 17:57:45 +0800

    login

--- src/page/user/Login/login.js
+++ src/page/user/Login/login.js
@@ -1,7 +1,14 @@
-import { Button } from 'antd';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Form, Input, Button, Checkbox } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons/lib";
 import styles from './style.less';
 
-export default () => {
+const NormalLoginForm = () => {
+  const onFinish = values => {
+    console.log('Received values of form: ', values);
+  };
+
   return (
     <div className={styles.main}>
       <div className={styles.header}>
@@ -10,7 +17,61 @@ export default () => {
           <Button className={styles.backHome} type="text"><a href="/">返回</a></Button>
         </div>
       </div>
-      <div>Login</div>
+      <div className={styles.login}>
+        <div className={styles.loginDetail}>
+          <Form
+            name="normal_login"
+            className="login-form"
+            initialValues={{
+              remember: true
+            }}
+            onFinish={onFinish}
+          >
+            <Form.Item
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Username!"
+                }
+              ]}
+            >
+              <Input
+                prefix={<UserOutlined className="site-form-item-icon" />}
+                placeholder="Username"
+              />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Password!"
+                }
+              ]}
+            >
+              <Input
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Item>
+            <Form.Item>
+              <Form.Item name="remember" valuePropName="checked" noStyle>
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+            </Form.Item>
+
+            <Form.Item>
+              <Button type="primary" htmlType="submit" className="login-form-button">
+                Log in
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
     </div>
   );
 }
+
+ReactDOM.render(<NormalLoginForm />, mountNode);
